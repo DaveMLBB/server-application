67c5ad8f94284eeaf15550f5d015b3b892bedbef
DaveMLBB <davideallega@gmail.com>
2022-12-07 18:14:32 +0100

    model "Employee" created

--- src/main/java/com/example/server/ServerApplication.java
+++ src/main/java/com/example/server/ServerApplication.java
@@ -1,9 +1,11 @@
 package com.example.server;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.server")
+@EnableAutoConfiguration
 public class ServerApplication {
 
 	public static void main(String[] args) {
