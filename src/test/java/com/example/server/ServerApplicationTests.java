6f5bf5584f6fb2998d60ede226fb00852587fee5
DaveMLBB <davideallega@gmail.com>
2022-12-07 18:40:36 +0100

    add Employee test created

--- src/test/java/com/example/server/ServerApplicationTests.java
+++ src/test/java/com/example/server/ServerApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.server;
 
+import com.example.server.dao.EmployeeDao;
+import com.example.server.models.Employee;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ServerApplicationTests {
 
+	@Autowired
+	private EmployeeDao employeeDao;
+
 	@Test
-	void contextLoads() {
+	void addEmployeeTest(){
+
+		Employee employee = new Employee();
+		employee.setName("InsertName");
+		employee.setLocation("InsertLocation");
+		employee.setBranch("InsertBranch");
+		employeeDao.save(employee);
 	}
 
 }
