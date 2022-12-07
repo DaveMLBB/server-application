12be2dc882dbca9f2dee9780340927fc653c004d
DaveMLBB <davideallega@gmail.com>
2022-12-07 18:33:01 +0100

    crud service EmployeeDao.java created

--- src/main/java/com/example/server/dao/EmployeeDao.java
+++ src/main/java/com/example/server/dao/EmployeeDao.java
@@ -1,12 +1,32 @@
 package com.example.server.dao;
 
+import com.example.server.models.Employee;
 import com.example.server.repo.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class EmployeeDao {
 
     @Autowired
     private EmployeeRepo employeeRepo;
+
+    public void save(Employee employee){
+        employeeRepo.save(employee);
+    }
+
+    public void delete(Employee employee){
+        employeeRepo.delete(employee);
+    }
+
+    public List<Employee> getAllEmployee(){
+        List<Employee> employees = new ArrayList<>();
+        Streamable.of(employeeRepo.findAll())
+                .forEach(employees::add);
+        return employees;
+    }
 }
