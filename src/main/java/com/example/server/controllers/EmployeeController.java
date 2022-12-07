9358eec10db608d41abd9fcb9a24d6a1bc2dfbe2
DaveMLBB <davideallega@gmail.com>
2022-12-07 19:06:17 +0100

    post request "save" Created

--- src/main/java/com/example/server/controllers/EmployeeController.java
+++ src/main/java/com/example/server/controllers/EmployeeController.java
@@ -3,19 +3,24 @@ package com.example.server.controllers;
 import com.example.server.dao.EmployeeDao;
 import com.example.server.models.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/employee")
 public class EmployeeController {
 
     @Autowired
     private EmployeeDao employeeDao;
 
-    @GetMapping("/employee/get-all")
+    @GetMapping("/get-all")
     public List<Employee> getAllEmployees(){
         return employeeDao.getAllEmployee();
+    }
+
+    @PostMapping("/save")
+    public void save(@RequestBody Employee employee){
+        employeeDao.save(employee);
     }
 }
